ab352be234d2c8299ee3e88f8b4ebbfb6993d591
GitHub Action <action@github.com>
2025-01-04 00:37:21 +0000

    Publish template output for Material

---
+++ BlazoriseBugReportMaterial_250104.Client/Components/TodoApp/Filter.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazoriseBugReportMaterial_250104.Client.Components.TodoApp;
+
+public enum Filter
+{
+    All,
+    Active,
+    Completed,
+}
---
+++ BlazoriseBugReportMaterial_250104.Client/Components/TodoApp/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazoriseBugReportMaterial_250104.Client.Components.TodoApp;
+
+public class Todo
+{
+    public bool Completed { get; set; }
+
+    public string? Description { get; set; }
+}
